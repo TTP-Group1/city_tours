3dafe4efb2957bb43c36ff7ed7078293d7f44039
Ankit69 <51173448+Ankit69@users.noreply.github.com>
2022-01-21 00:09:08 -0500

    commit

--- src/tourData.js
+++ src/tourData.js
@@ -1,12 +1,11 @@
 export const tourData = [
     {
-        // Thin Thazin commented
         id: 1,
-        city: "Villisca, Iowa",
-        img: "./img/Villisca.png",
-        name: "The Villisca Axe Murder House",
+        city: "new york",
+        img: "./img/newyork.jpg",
+        name: "Statue of Liberty tour",
         info: 
-        "On June 10, 1912, Josiah and Sarah Moore were bludgeoned to death inside of their home in Villisca, Iowa. Their four children—and two friends who were spending the night—were also killed, and to this day, the crime remains unsolved. Their home is considered one of the most haunted houses in the country, and guests are drawn to it. People even pay $400+ to stay for one night." 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
     },
     {
         id: 2,
@@ -34,22 +33,22 @@ export const tourData = [
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
     },
     {
-        //Ankit Kafle
         id: 5,
+        city: "Brooklyn Bridge",
+        img: "./img/bkbridge.jpg",
+        name: "Brooklyn Bridge",
+        info: 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
+    },
+    {
+        //Ankit Kafle
+        id: 6,
         city: "Hot Springs, Virginia",
         img: "./img/OmniHomestead.jpg",
         name: "Omni Homestead",
         info: 
         "Here lies a timeless story. In the oldest wing of one of the oldest resorts in the country (built in 1766), a jilted woman took her own life after her fiancé left her on their wedding day and never returned. Since that tragic day in the early 1900s she's wandered the 14th floor aimlessly, stopping guests and employees to ask for the time, hoping that she'll catch the hour when her groom-to-be is supposed to come back to her—and that this time around he'll be there." 
-   },
-    {
-        id: 6,
-        city: "Yankee Stadium",
-        img: "./img/yankeestadium.jpg",
-        name: "Yankee Stadium",
-        info: 
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
-    },
+        },
     {
         id: 7,
         city: "Yankee Stadium",
